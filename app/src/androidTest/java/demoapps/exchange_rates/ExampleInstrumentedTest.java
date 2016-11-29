6a189db68760c91cb4d0ffe6f4ded0c9654fc7a8
Erdem OLKUN <erdemolkun@gmail.com>
2016-11-29 09:20:00 +0300

    Add turkish localization.

--- app/src/androidTest/java/demoapps/exchange_rates/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/demoapps/exchange_rates/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("demoapps.exchangegraphics", appContext.getPackageName());
+        assertEquals("demoapps.exchange_rates", appContext.getPackageName());
     }
 }
