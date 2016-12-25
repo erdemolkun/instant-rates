4fe2ecb66328aafe8a93e56fd49ac70c01e63e72
Erdem OLKUN <erdemolkun@gmail.com>
2016-12-25 16:14:28 +0300

    Cleanup.

--- app/src/main/java/dynoapps/exchange_rates/util/Formatter.java
+++ app/src/main/java/dynoapps/exchange_rates/util/Formatter.java
@@ -1,7 +1,6 @@
 package dynoapps.exchange_rates.util;
 
 import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
 import java.util.Locale;
 
@@ -10,21 +9,10 @@ import java.util.Locale;
  */
 
 public class Formatter {
-    private DecimalFormat mFormat;
+
     NumberFormat numberFormat;
 
     public Formatter(int digits) {
-        char groupingSeparator = DecimalFormatSymbols.getInstance(Locale.getDefault()).getGroupingSeparator();
-        char decimalSeparator = DecimalFormatSymbols.getInstance(Locale.getDefault()).getDecimalSeparator();
-        StringBuffer b = new StringBuffer();
-        for (int i = 0; i < digits; i++) {
-            if (i == 0)
-                b.append(decimalSeparator);
-            b.append("0");
-        }
-
-        //mFormat = new DecimalFormat("###" + groupingSeparator + "###" + groupingSeparator + "###" + groupingSeparator + "##0" + b.toString());
-//        mFormat = new DecimalFormat("###,###,###,##0" + b.toString());
         numberFormat = DecimalFormat.getInstance(Locale.getDefault());
         numberFormat.setMaximumFractionDigits(digits);
 
