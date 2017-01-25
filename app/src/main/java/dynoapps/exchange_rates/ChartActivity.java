66687f98a2b6ba45c3fe17bbcb528d647219cbe8
Erdem OLKUN <eolkun@protel.com.tr>
2017-01-25 17:43:25 +0300

    Add license to template.

--- app/src/main/java/dynoapps/exchange_rates/ChartActivity.java
+++ app/src/main/java/dynoapps/exchange_rates/ChartActivity.java
@@ -61,9 +61,18 @@ import dynoapps.exchange_rates.util.RateUtils;
 import dynoapps.exchange_rates.util.ViewUtils;
 
 /**
- * Created by erdemmac on 24/11/2016.
+ * Copyright 2016 Erdem OLKUN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
-
 public class ChartActivity extends BaseActivity {
 
     public static final String EXTRA_RATE_TYPE = "EXTRA_RATE_TYPE";
--- app/src/main/java/dynoapps/exchange_rates/converters/DolarTlKurAjaxConverter.java
+++ app/src/main/java/dynoapps/exchange_rates/converters/DolarTlKurAjaxConverter.java
@@ -15,7 +15,17 @@ import retrofit2.Converter;
 import retrofit2.Retrofit;
 
 /**
- * Created by erdemmac on 24/11/2016.
+ * Copyright 2016 Erdem OLKUN
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 public class DolarTlKurAjaxConverter implements Converter<ResponseBody, List<BaseRate>> {
