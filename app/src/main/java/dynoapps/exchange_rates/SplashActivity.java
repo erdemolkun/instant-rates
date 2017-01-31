9462d0f852ecfff9376c344f9045af5d07a6a8ba
Erdem OLKUN <erdemolkun@gmail.com>
2017-01-31 13:25:58 +0300

    New wing vector.

--- app/src/main/java/dynoapps/exchange_rates/SplashActivity.java
+++ app/src/main/java/dynoapps/exchange_rates/SplashActivity.java
@@ -1,10 +1,14 @@
 package dynoapps.exchange_rates;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
 import android.app.ActivityManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Path;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,8 +16,9 @@ import android.os.IBinder;
 import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
 import org.greenrobot.eventbus.EventBus;
@@ -32,7 +37,7 @@ import dynoapps.exchange_rates.util.L;
  */
 
 public class SplashActivity extends AppCompatActivity {
-    private static final int MIN_DURATION = 450;
+    private static final int MIN_DURATION = 8000;
 
     Handler handler;
 
@@ -69,23 +74,48 @@ public class SplashActivity extends AppCompatActivity {
 //        if (drawable instanceof Animatable) {
 //            ((Animatable) drawable).start();
 //        }
+        ivLogo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoNextIntent();
+            }
+        });
 
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
-            AnimatedVectorDrawableCompat animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_wings);
+            animatedVectorDrawableCompat = AnimatedVectorDrawableCompat.create(this, R.drawable.avd_wings_2);
             ivLogo.setImageDrawable(animatedVectorDrawableCompat);
             animatedVectorDrawableCompat.start();
         } else {
             ivLogo.setImageResource(R.drawable.ic_store_icon_24dp);
         }
+        final Path path = new Path();
+        path.quadTo(30, -60, 0, -240);
+        path.quadTo(30, -60, 0, 0);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(ivLogo, View.TRANSLATION_X, View.TRANSLATION_Y, path).
+                setDuration(800);
+        objectAnimator.setInterpolator(new LinearInterpolator());
+        objectAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (animatedVectorDrawableCompat != null) {
+                    animatedVectorDrawableCompat.stop();
+                }
+            }
+        });
+        //objectAnimator.start();
+
+
     }
 
+    AnimatedVectorDrawableCompat animatedVectorDrawableCompat;
 
     private void onConnectionDone() {
         long currentMilis = System.currentTimeMillis();
         if (currentMilis - startMilis < MIN_DURATION) {
-            handler.postDelayed(runnable, Math.abs(MIN_DURATION - (currentMilis - startMilis)));
+            //handler.postDelayed(runnable, Math.abs(MIN_DURATION - (currentMilis - startMilis)));
         } else {
-            gotoNextIntent();
+//            gotoNextIntent();
         }
     }
 
